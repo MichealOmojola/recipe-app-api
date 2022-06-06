3f69386f6a5c27557478e7e03a0b203c5c4c44a8
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-06 21:47:28 +0100

    pushed

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -24,8 +24,7 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [psycopg2Error] * 2 \
             + [OperationalError] * 3 + [True]
-        call_command('wait_for_db')
-        
+        call_command('wait_for_db')  
+    
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-            
