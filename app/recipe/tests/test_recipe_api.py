8a04b1985c629fd8a801375852ca32071b8787be
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-06-08 17:05:48 +0100

    pushed

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -12,9 +12,17 @@ from rest_framework import status
 
 from core.models import Recipe
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer,
+)
+
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+def detail_url(recipe_id):
+    """Create and return a recipe detail URL."""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 def create_recipe(user, **params):
     """Create and return a sample recipe."""
@@ -52,6 +60,7 @@ class PrivateRecipeAPITests(TestCase):
             'user@example.com',
             'testpass123',
         )
+        self.client.force_authenticate(self.user)
 
     def test_retrieve_recipes(self):
         """Test requiving a list of recipes."""
@@ -78,4 +87,14 @@ class PrivateRecipeAPITests(TestCase):
         recipes = Recipe.objects.filter(user = self.user)
         serializer = RecipeSerializer(recipes, many = True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """Test get recipes detail."""
+        recipe = create_recipe(user=self.user)
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
